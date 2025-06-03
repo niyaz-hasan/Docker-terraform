1da00fe3b2d17f177209e4607a3d54a4e727513b
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-03 13:17:05 +0530

    added small change

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -89,6 +89,6 @@ output "public_subnets" {
   value = aws_subnet.public[*].id
 }
 
-output "private_subnets" {
-  value = aws_subnet.private[*].id
-}
+#output "private_subnets" {
+#  value = aws_subnet.private[*].id
+#}
